0e48acd9957520ec99bb8c7bde9d4c233716601c
CristinaResina <resina.cristina96@gmail.com>
2023-07-07 20:44:46 -0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+let vowels = "aelouAEIOU";
 
+function translateToPiglatin(){
+  // Declare local variables
+  let input = inputText.value; // Stores the value of the input text 
+  let piglatin =''; //Placeholder for the pig Latin output
+
+  if (input.length <= 1) {
+    // The string contains only one letter or less (invalid)
+    piglatin = "Invalid input. A word must have more than one letter.";
+  }else if ( !vowels.includes(input.charAt(0)) && !vowels.includes(input.charAt(1))){
+    // The first two letters are consonants (not vowels)
+    pigLatin = input.slice(2) + input.slice(0, 2) + 'ay';
+  } else if (!vowels.includes(input.charAt(0))){
+    //The first letter is a consonant
+    pigLatin = input.slice(1) + input.charAt(0) + 'ay';
+  }else{
+    // The first letter is a vowel
+    pigLatin = input + 'way';
+  }
+
+  // Update the text in the pigoutput span
+  translationResult. innerHTML - piglatin;
+    
+};
+  
+
+
+
+
+
+
+
+
+
+
+  
