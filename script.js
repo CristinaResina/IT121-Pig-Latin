576cf582afb794d93d07991e58c96caa34bbab68
CristinaResina <resina.cristina96@gmail.com>
2023-07-07 21:15:14 -0700

    Update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let vowels = "aelouAEIOU";
 
-function translateToPiglatin(){
+function translateToPigLatin(){
   // Declare local variables
   let input = inputText.value; // Stores the value of the input text 
-  let piglatin =''; //Placeholder for the pig Latin output
+  let pigLatin =''; //Placeholder for the pig Latin output
 
   if (input.length <= 1) {
     // The string contains only one letter or less (invalid)
-    piglatin = "Invalid input. A word must have more than one letter.";
-  }else if ( !vowels.includes(input.charAt(0)) && !vowels.includes(input.charAt(1))){
+    pigLatin = "Invalid input. A word must have more than one letter.";
+  }else if (!vowels.includes(input.charAt(0)) && !vowels.includes(input.charAt(1))){
     // The first two letters are consonants (not vowels)
     pigLatin = input.slice(2) + input.slice(0, 2) + 'ay';
   } else if (!vowels.includes(input.charAt(0))){
@@ -20,7 +20,7 @@ function translateToPiglatin(){
   }
 
   // Update the text in the pigoutput span
-  translationResult. innerHTML - piglatin;
+  translationResult.innerHTML = pigLatin;
     
 };
   
